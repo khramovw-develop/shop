d5e049115e8d3cae58d03bba20d4f6f6136c4dd1
khramov <khramovw.develop@gmail.com>
2022-04-25 21:27:17 +0300

    202 Multiple provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/pages/products/product-detail.screen.dart';
+import 'package:shop/providers/cart/cart.provider.dart';
 import 'package:shop/providers/product/products.provider.dart';
 import 'package:shop/pages/products/products-overview.screen.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<Products>(create: (_) => Products()),
+          ChangeNotifierProvider<Cart>(create: (_) => Cart()),
+        ],
+        child: const MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Products>(
-      create: (_) => Products(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: buildThemeData,
-        initialRoute: ProductsOverviewScreen.routeName,
-        routes: {
-          ProductsOverviewScreen.routeName: (ctx) => const ProductsOverviewScreen(),
-          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
-        },
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: buildThemeData,
+      initialRoute: ProductsOverviewScreen.routeName,
+      routes: {
+        ProductsOverviewScreen.routeName: (ctx) => const ProductsOverviewScreen(),
+        ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+      },
     );
   }
 
